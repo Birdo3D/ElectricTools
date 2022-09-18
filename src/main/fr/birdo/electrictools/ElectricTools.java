0b44c9d197a5fd8ed6f89b2e77a2555a43cce622
Birdo3D <52548243+Birdo3D@users.noreply.github.com>
2022-09-18 19:30:18 +0200

    Guis updating

--- src/main/fr/birdo/electrictools/ElectricTools.java
+++ src/main/fr/birdo/electrictools/ElectricTools.java
@@ -12,36 +12,32 @@ public class ElectricTools extends JPanel {
     public static Mode mode = Mode.DARK_MODE;
 
     public static void main(String[] args) {
+        Thread newThread = new Thread(ElectricTools::refresh);
+        newThread.start();
         GuiUtilities.openGui(Guis.loadingGui);
+        //Loading
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         GuiUtilities.closeGui(Guis.loadingGui);
-        //GuiUtilities.openGui(Guis.startGui);
-        GuiUtilities.openGui(Guis.switchboardGui);
-        refresh();
+        GuiUtilities.openGui(Guis.startGui);
     }
 
     @SuppressWarnings("BusyWait")
     public static void refresh() {
+        int stop = 0;
         while (true) {
             try {
                 Thread.sleep(1000 / 30);
                 if (GuiUtilities.getEnableGuis().isEmpty())
-                    System.exit(2410);
-                for (Gui gui : GuiUtilities.getEnableGuis()) {
-                    GuiUtilities.updateGui(gui);
-                    /*if (gui.isMaximized()) {
-                        Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration());
-                        if ((insets.top > 0 && gui.getPosY() < 0) &&
-                                (insets.left > 0 && gui.getPosX() < 0) &&
-                                (insets.right > 0 && gui.getPosX() == 0 && Toolkit.getDefaultToolkit().getScreenSize().getWidth() - gui.getSizeX() > 0) &&
-                                (insets.bottom > 0 && gui.getPosY() == 0 && Toolkit.getDefaultToolkit().getScreenSize().getHeight() - gui.getSizeY() > 0))
-                            GuiUtilities.maximizeGui(gui);
-                    }*/
-                }
+                    stop++;
+                if (stop > 90)
+                    System.exit(0);
+                for (Gui gui : GuiUtilities.getEnableGuis())
+                    if (gui != null && gui.getFrame() != null)
+                        GuiUtilities.updateGui(gui);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
--- src/main/fr/birdo/electrictools/guis/GuiStart.java
+++ src/main/fr/birdo/electrictools/guis/GuiStart.java
@@ -12,6 +12,8 @@ public class GuiStart extends Gui {
         super("gui_start", "Welcome to ElectricTools !", 800, 400);
         setContentPane(this);
         setToolbar(new ToolBar(this, 30));
+        //New switchboard button
+        addButton(new Button(0, 60, 60, 80, 80));
     }
 
     public void paintComponent(Graphics g) {
@@ -20,9 +22,32 @@ public class GuiStart extends Gui {
         g.fillRect(0, 0, (int) this.getFrame().getSize().getWidth(), (int) this.getFrame().getSize().getHeight());
         //Top Bar
         getToolBar().drawToolbar();
+        //Buttons
+        for (Button button : this.getButtons()) {
+            switch (button.getId()) {
+                case 0:
+                    if (button.isHover()) {
+                        g.setColor(Color.magenta);
+                        g.fillRoundRect(60, 60, 80, 80, 14, 14);
+                    }
+                    g.setColor(Color.YELLOW);
+                    g.fillRoundRect(80, 80, 40, 40, 14, 14);
+                    break;
+            }
+        }
     }
 
     public void buttonClicked(Button button, int mouseButton, int clickCount) {
-
+        switch (button.getId()) {
+            case 0:
+                GuiUtilities.closeGui(this);
+                try {
+                    Thread.sleep(1000 / 5);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                GuiUtilities.openGui(Guis.switchboardGui);
+                break;
+        }
     }
 }
